d26dbfbb0dcd25ab21e1155ff6d4759b61ac2aa9
Unknown <css_mn@yahoo.ie>
2019-01-24 11:50:27 +0100

    Showed zmod module structure

--- src/finite_field.lean
+++ src/finite_field.lean
@@ -16,8 +16,8 @@ sorry
 lemma ring_char_prime [fintype α] : nat.prime (ring_char α) :=
 sorry
 
---set_option pp.notation false
---set_option pp.implicit true
+lemma dvd_self {α : Type u} [has_dvd α] {a : α} : a ∣ a := sorry
+
 instance zmod_ring_hom {h : ring_char α > 0} :
 is_ring_hom (nat.cast ∘ fin.val : zmod ⟨ring_char α, h⟩ → α) :=
 { map_one := have h1 : (1 : ℕ) < (⟨ring_char α, h⟩ : ℕ+),
@@ -29,35 +29,34 @@ is_ring_hom (nat.cast ∘ fin.val : zmod ⟨ring_char α, h⟩ → α) :=
       rw[←nat.cast_one, zmod.val_cast_of_lt h1],
       exact nat.cast_one
     end,
-  map_mul := λ {x y}, show (↑(x * y).val : α) = ↑x.val * ↑y.val,     
+  map_mul := λ {x y}, show (↑(x * y).val : α) = ↑x.val * ↑y.val, from
     begin
       rw[←nat.cast_mul],
+      rw[←nat.mod_add_div(x.val * y.val) (ring_char α)],
       rw[zmod.mul_val],
-      --rw[subtype.coe_mk (ring_char α) h],
-      sorry
+      simp,
+      rw[(ring_char.spec α (ring_char α)).mpr dvd_self],
+      rw[zero_mul, add_zero]
     end,
-  map_add := λ x y, sorry }
-
-instance zmod_smul_ring_hom {h : ring_char α > 0} {r : zmod ⟨ring_char α, h⟩} :
-is_ring_hom (λ x : α, (nat.cast ∘ fin.val) r * x) := sorry
-
---noncomputable instance zmod_scalar_pos_char {h : ring_char α > 0} :
---has_scalar (zmod ⟨ring_char α, h⟩) α :=
---{ smul := λ n a, (nat.cast ∘ fin.val) n * a }
+  map_add := λ x y, show (↑(x + y).val : α) = ↑x.val + ↑y.val, from
+    begin
+      rw[←nat.cast_add],
+      rw[←nat.mod_add_div(x.val + y.val) (ring_char α)],
+      rw[zmod.add_val],
+      simp,
+      rw[(ring_char.spec α (ring_char α)).mpr dvd_self],
+      rw[zero_mul, add_zero]
+    end }
 
 open is_ring_hom
-
---set_option pp.notation false
 
 noncomputable instance zmod_module_pos_char {h : ring_char α > 0} :
 module (zmod ⟨ring_char α, h⟩) α :=
 module.of_core
 { smul := λ r x, (nat.cast ∘ fin.val) r * x,
-  smul_add := λ r x y, map_add _,
-  add_smul := λ r s x, show (nat.cast ∘ fin.val) (r + s) * x = _,
-    by rw[@map_add _ _ _ _ (nat.cast ∘ fin.val) zmod_ring_hom _ _, add_mul], 
-  mul_smul := λ r s x, show (nat.cast ∘ fin.val) (r * s) * x = _,
-    by rw[@map_mul _ _ _ _ (nat.cast ∘ fin.val) zmod_ring_hom _ _, mul_assoc], 
+  smul_add := λ r x y, by unfold has_scalar.smul; rw[mul_add]; refl,
+  add_smul := λ r s x, by unfold has_scalar.smul; rw[@map_add _ _ _ _ (nat.cast ∘ fin.val) zmod_ring_hom _ _, add_mul],
+  mul_smul := λ r s x, by unfold has_scalar.smul; rw[@map_mul _ _ _ _ (nat.cast ∘ fin.val) zmod_ring_hom _ _, mul_assoc], 
   one_smul := λ x, show (nat.cast ∘ fin.val) 1 * x = _,
     by rw[@map_one _ _ _ _ (nat.cast ∘ fin.val) zmod_ring_hom, one_mul] }
 
