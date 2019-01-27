878007f6822ad5a757f0461cc7c673a31ed6fa3b
Unknown <css_mn@yahoo.ie>
2019-01-27 23:49:22 +0100

    WIP

--- src/vector_space.lean
+++ src/vector_space.lean
@@ -5,9 +5,11 @@ universes u v
 
 namespace finsupp
 
+open finset
+
 variables {α : Type u} {β : Type v}
 variables [decidable_eq α]
-variables [decidable_eq β] [has_zero β]
+variables [decidable_eq β] [add_comm_monoid β]
 variables (p : α → Prop) [d : decidable_pred p]
 
 variable (s : finset α)
@@ -15,75 +17,33 @@ variable (s : finset α)
 def finsupp_equiv_fintype_domain [h : fintype α] : (α →₀ β) ≃ (α → β) :=
 { to_fun := finsupp.to_fun,
   inv_fun := λ f, finsupp.mk (finset.filter (λ a, f a ≠ 0) h.elems) f
-    (assume a, by rw[finset.mem_filter]; apply and_iff_right; exact fintype.complete a),
-  left_inv := λ f, finsupp.ext (λ a, rfl),
-  right_inv := λ f, funext (λ a, rfl) }
+    (assume a, by rw[mem_filter]; exact and_iff_right (fintype.complete a)),
+  left_inv := λ f, finsupp.ext (λ _, rfl),
+  right_inv := λ f, rfl }
+
+lemma map_domain_apply {α₁ α₂ : Type*} [decidable_eq α₁] [decidable_eq α₂]
+(v : α₁ → α₂) (f : α₁ →₀ β) (h : function.injective v) {a : α₁} :
+(map_domain v f) (v a) = f a :=
+sorry
 
 include d
 
-def extend_by_zero (f : subtype p → β) : α → β := λ a, if hc : p a then f ⟨a, hc⟩ else 0
+lemma extend_restrict (f : {f : α →₀ β // ∀ a ∈ f.support, p a}) :
+map_domain subtype.val (subtype_domain p f.val) = f :=
+finsupp.ext $ λ a, sorry
 
-/-- `subtype_domain_extend f` is the extension of the finitely supported function
-  `f` on the subtype `p` to the finitely supported function by extending by zero. -/
-def subtype_domain_extend (f : subtype p →₀ β) : α →₀ β :=
-{ support            := finset.map ⟨subtype.val, subtype.val_injective⟩ f.support,
-  to_fun             := extend_by_zero p f,
-  mem_support_to_fun := λ a,
-    iff.intro
-      (assume hmem,
-      let ⟨ap, hsup, hs⟩ := finset.mem_map.mp hmem in
-      have hp : p a, from hs ▸ ap.property,
-      have ap = ⟨a, hp⟩, by rwa[subtype.ext],
-      by unfold extend_by_zero; rw [dif_pos hp, ←this]; exact (mem_support_to_fun f ap).mp hsup)
-      (assume hne0 : dite _ _ _ ≠ 0,
-      have hp : p a, from decidable.by_contradiction (assume hnp, absurd (dif_neg hnp) hne0),
-      have h : f ⟨a, hp⟩ ≠ 0, by rwa [dif_pos] at hne0,
-      finset.mem_map_of_mem _ ((mem_support_to_fun f ⟨a, hp⟩).mpr h)) }
+lemma restrict_extend (f : subtype p →₀ β) :
+subtype_domain p (map_domain subtype.val f) = f :=
+finsupp.ext $ λ a, map_domain_apply _ _ (subtype.val_injective)
 
-def subtype_domain_extend' [add_comm_monoid β] (f : subtype p →₀ β) : α →₀ β :=
-sorry --map_domain (@subtype.val α p) f
-
-lemma subtype_domain_extend_apply {f : subtype p →₀ β} {a : subtype p} :
-(subtype_domain_extend p f) (a.val) = f a := 
-by rw[←subtype.coe_eta a _]; exact dif_pos a.property
-
-lemma subtype_domain_extend_restrict (f : α →₀ β) (h : ∀ a ∈ f.support, p a) :
-subtype_domain_extend p (subtype_domain p f) = f :=
-finsupp.ext
-  (assume a,
-  match d a with
-    | is_false hna :=
-      have a ∉ f.support, from assume hs, absurd (h a hs) hna,
-      have hf : f a = 0, from not_mem_support_iff.mp this,
-      have hg : (subtype_domain_extend p (subtype_domain p f)) a = 0, from dif_neg hna,
-      by rw[hf, hg]
-    | is_true ha := have a = (subtype.mk a ha).val, from rfl,
-      by rw[this, subtype_domain_extend_apply p, subtype_domain_apply]
-  end)
-
-lemma subtype_domain_restrict_extend (f : {a // p a} →₀ β) :
-subtype_domain p (subtype_domain_extend p f) = f :=
-by apply finsupp.ext; intro a; rw[subtype_domain_apply, subtype_domain_extend_apply p]
-
-example (s : set α) : {f : α →₀ β // ↑f.support ⊆ s} ≃ s →₀ β := sorry
-
-example : {f : α →₀ β // ∀ a : α, f a ≠ 0 → p a} ≃ subtype p →₀ β := sorry
-
-set_option eqn_compiler.zeta true
-
-def finnsup_equiv_subtype_domain : {f : α →₀ β // ∀ a ∈ f.support, p a} ≃ ({a // p a} →₀ β) :=
-{ to_fun    := (finsupp.subtype_domain p) ∘ subtype.val,
-  inv_fun   := λ f,
-    let g := (subtype_domain_extend p f) in
-    subtype.mk g
-      (assume a (h : a ∈ g.support),
-      decidable.by_contradiction 
-        (assume hnp : ¬p a,
-        have g a = 0, from dif_neg hnp,
-        have hn : a ∉ g.support, from not_mem_support_iff.mpr this,
-        absurd h hn)),
-  left_inv := λ f, by rw[subtype.ext]; exact subtype_domain_extend_restrict _ f.val f.property,
-  right_inv := λ f, subtype_domain_restrict_extend _ f }
+def finnsup_equiv_subtype_domain : {f : α →₀ β // ∀ a ∈ f.support, p a} ≃ (subtype p →₀ β) :=
+{ to_fun := (finsupp.subtype_domain p) ∘ subtype.val,
+  inv_fun := λ f, ⟨map_domain subtype.val f,
+    assume a h,
+    have h0 : a ∈ image _ _, from mem_of_subset map_domain_support h,
+    let ⟨ap, _, hs⟩ := mem_image.mp h0 in hs ▸ ap.property⟩,
+  left_inv := λ f, subtype.eq $ extend_restrict _ f,
+  right_inv := restrict_extend _ }
 
 end finsupp
 
